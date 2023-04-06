0de5acfb9ae064021cad70ad3d4062a6bb55ca9c
ieviev <ieviev@users.noreply.github.com>
2023-04-07 01:05:51 +0300

    .

--- __functions.psm1
+++ __functions.psm1
@@ -20,10 +20,12 @@ function lsr()
         $args
     )
     $items = Get-ChildItem -Recurse $args 
+        | ForEach-Object{$_.FullName}
     return $items
 }
 
 Set-Alias -Name 'clip'  -Value Set-Clipboard
+## ex. ps python | kill
 Set-Alias -Name "ps"    -Value Get-Process
 Set-Alias -Name "kill"  -Value Stop-Process
 Set-Alias -Name 'map'  -Value %
