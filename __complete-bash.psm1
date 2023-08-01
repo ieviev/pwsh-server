3277bb8a2bbf0211be73711536d4604c1f433569
ieviev <ieviev@users.noreply.github.com>
2023-08-01 14:34:31 +0300

    more bash completions

--- __complete-bash.psm1
+++ __complete-bash.psm1
@@ -2,9 +2,28 @@
 Register-ArgumentCompleter `
     -Native `
     -CommandName @( 
-        "git","docker","rclone", "systemctl", 
-        "hcitool", "flatpak", "scp", "ssh",
-        "journalctl", "pacmd", "pactl", "gh"
+        "apt",
+        "dd", 
+        "docker",
+        "du", 
+        "find", 
+        "flatpak", 
+        "gh",
+        "git",
+        "gpg",
+        "hcitool", 
+        "ip", 
+        "journalctl", 
+        "ldd"
+        "lzma"
+        "nc",
+        "pacmd", 
+        "pactl", 
+        "rclone", 
+        "scp", 
+        "ssh",
+        "systemctl", 
+        "tar"
     ) `
     -ScriptBlock {
     param($commandName, [System.Management.Automation.Language.CommandAst] $commandAst, $cursorPosition)
@@ -12,8 +31,18 @@ Register-ArgumentCompleter `
     if ($cursorPosition -gt $commandAst.Extent.Text.Length) {
         $bashcomplete += " "
     }
-    bash $PSScriptRoot/get_bashcompletions.sh $bashcomplete
-    | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    $defaultResults = 
+        bash $PSScriptRoot/get_bashcompletions.sh $bashcomplete
+        | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+    switch ("$commandAst") {
+        "tar" {
+            $defaultResults += [System.Management.Automation.CompletionResult]::new("-xvzf", "-xvzf", 'ParameterValue', "-xvzf")
+            $defaultResults += [System.Management.Automation.CompletionResult]::new("-cvzf", "-cvzf", 'ParameterValue', "-cvzf")
+            $defaultResults;
+        }
+        default { $defaultResults; }
     }
 }
+ 
